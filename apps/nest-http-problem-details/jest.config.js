735d8b2450ad0d303ce4b95d46c0946f81b4de8c
Abdeldjalil Fortas <dr.fcmam5@gmail.com>
2021-06-05 17:40:50 +0200

    :rocket: initialize project with NX

---
+++ apps/nest-http-problem-details/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'nest-http-problem-details',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nest-http-problem-details',
+};
